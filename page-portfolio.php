61a811e7471d77e2d8e70407cb4b04fd6a5a06f8
Michael Hemingway <mikemingway@gmail.com>
2016-11-09 14:24:57 -0500

    Layout changes and formatting of the byline

--- page-portfolio.php
+++ page-portfolio.php
@@ -5,24 +5,41 @@
 		<div id="inner-content" class="row">
 
 		    <main id="main" class="grid" role="main">
+					<?php
+					$the_query = new WP_Query( array( 'cat' => get_cat_id('portfolio')) ); ?>
 
-				<?php
-					$wp_query = new WP_Query(array(
-						'category_name' => 'portfolio',
-						'showposts' => 20,
-						'paged' => true
-					));
+					<?php if ( $the_query->have_posts() ) : ?>
 
-					while ($wp_query->have_posts()) : $wp_query->the_post();
-						get_template_part( 'parts/loop', 'grid' );
-					endwhile;
-					wp_reset_postdata();
-				?>
+						<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+							<a href="<?php the_permalink() ?>" class="grid-item">
+								<div id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="image" itemscope itemtype="http://schema.org/image">
+									<h3 class="grid-image-title"><?php the_title(); ?></h1>
+									<?php the_post_thumbnail('large');?>
+								</div>
+							</a>
+						<?php endwhile; ?>
 
-			</main> <!-- end #main -->
+						<!-- pagination here -->
 
-		</div> <!-- end #inner-content -->
+						<?php wp_reset_postdata(); ?>
 
-	</div> <!-- end #content -->
+					<?php else : ?>
+						<p><?php __( 'Sorry, no posts matched your criteria.' ); ?></p>
+					<?php endif; ?>
 
-<?php get_footer(); ?>
+			</main>
+
+		</div>
+
+	</div>
+	<footer class="footer" role="contentinfo">
+		<div id="inner-footer" class="row">
+			<div class="large-12 medium-12 columns">
+				<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
+			</div>
+		</div> <!-- end #inner-footer -->
+	</footer> <!-- end .footer -->
+
+<?php wp_footer(); ?>
+</body>
+</html> <!-- end page -->
